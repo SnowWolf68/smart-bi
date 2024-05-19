a67e80a4626d4a24d189b2d42414c791939c26d3
SnowWolf68 <zhangjiashuo0818@163.com>
2024-05-19 09:23:12 +0800

    add: 添加swagger接口文档生成功能

--- src/main/java/com/snwolf/bi/config/MvcConfig.java
+++ src/main/java/com/snwolf/bi/config/MvcConfig.java
@@ -2,13 +2,22 @@ package com.snwolf.bi.config;
 
 import com.snwolf.bi.interceptor.LoginInterceptor;
 import com.snwolf.bi.interceptor.RefreshTokenInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+@Slf4j
 public class MvcConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -17,10 +26,31 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/user/**")
                 .excludePathPatterns(
                         "/user/login",
                         "/user/register"
                 ).order(1);
         registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).order(0);
+    }
+
+    /**
+     * 通过knife4j生成接口文档
+     * @return
+     */
+    @Bean
+    public Docket docket() {
+        log.info("准备生成接口文档");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("smart-bi项目接口文档")
+                .version("1.0")
+                .description("smart-bi项目接口文档")
+                .build();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.snwolf.bi.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
--- src/main/java/com/snwolf/bi/controller/UserController.java
+++ src/main/java/com/snwolf/bi/controller/UserController.java
@@ -7,20 +7,23 @@ import com.snwolf.bi.domain.dto.UserRegisterDTO;
 import com.snwolf.bi.domain.entity.User;
 import com.snwolf.bi.result.Result;
 import com.snwolf.bi.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Api(tags = "用户相关接口")
 public class UserController {
 
     private final IUserService userService;
 
+    @ApiOperation("用户注册")
     @PostMapping("/register")
     public Result register(@RequestBody UserRegisterDTO userRegisterDTO){
         userService.register(userRegisterDTO);
@@ -28,12 +31,14 @@ public class UserController {
     }
 
     @PostMapping("/login")
+    @ApiOperation("用户登录")
     public Result<String> login(@RequestBody UserLoginDTO userLoginDTO){
         String token = userService.login(userLoginDTO);
         return Result.success(token);
     }
 
     @GetMapping("/list")
+    @ApiOperation("查询用户列表")
     public Result<List<UserDTO>> list(){
         List<User> userList = userService.list();
         List<UserDTO> userDTOList = userList.stream().map(user -> BeanUtil.copyProperties(user, UserDTO.class)).collect(Collectors.toList());
